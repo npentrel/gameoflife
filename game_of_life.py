5fd41d9cbde080f250aff221f631d23590932bec
npentrel <npentrel@gmail.com>
2017-10-15 01:43:01 +0200

    Add curses library

--- game_of_life.py
+++ game_of_life.py
@@ -1,18 +1,30 @@
+import math
+import time
+
 ALIVE = 1
 DEAD = 0
 
 class GameOfLife:
     """An implementation of the popular Game of Life simulation"""
 
-    def __init__(self, iterations, width, height, state):
+    def __init__(self, iterations, width, height, state, stdscr=None, color=None):
         self._iterations = iterations
         self._width = width
         self._height = height
         self._state = state
+        self._stdscr = stdscr
+        self._color = color
+        if self._stdscr:
+            self._alive_char = '* '
+            self._dead_char = '  '
+        else:
+            self._alive_char = '1 '
+            self._dead_char = '0 '
 
     def simulate(self):
         self._print_board()
         for _ in range(self._iterations):
+            time.sleep(1)
             self._apply_rules()
             self._print_board()
 
@@ -49,10 +61,25 @@ class GameOfLife:
         return count
 
     def _print_board(self):
+        if self._stdscr:
+            return self._curses_board()
         output = ""
         for y in range(self._height):
             for x in range(self._width):
-                output += '1 ' if (x, y) in self._state else '0 '
+                output += self._alive_char if (x, y) in self._state else self._dead_char
             output.rstrip()
             output += '\n'
         print(output)
+
+    def _curses_board(self):
+        scrheight, scrwidth = iter(self._stdscr.getmaxyx())
+        output = "\n" * ((scrheight - self._height)//2)
+        for y in range(self._height):
+            output += ' ' * int(math.ceil(scrwidth/2 - self._width))
+            for x in range(self._width):
+                output += '* ' if (x, y) in self._state else '  '
+            output.rstrip()
+            output += '\n'
+        self._stdscr.clear()
+        self._stdscr.addstr(output, self._color)
+        self._stdscr.refresh()
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import argparse
+import curses
+
 from game_of_life import GameOfLife
 
 
-def main():
-    iterations, width, height, state = _read_input()
-    game = GameOfLife(iterations, width, height, state)
-    game.simulate()
+def main(args):
+    try:
+        stdscr = None
+        color = None
+
+        iterations, width, height, state = _read_input()
+
+        if args.curses:
+            stdscr = curses.initscr()
+            curses.curs_set(0)
+            curses.start_color()
+            curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+            color = curses.color_pair(1)
+
+        game = GameOfLife(iterations, width, height, state, stdscr, color)
+        game.simulate()
+        if stdscr:
+            curses.endwin()
+    except KeyboardInterrupt:
+        try:
+            curses.endwin()
+        except curses.error:
+            pass
 
 
 def _get_input():
@@ -20,8 +42,14 @@ def _read_input():
         row = _get_input()
         state.update({(x, y) for x in range(width) if next(row)})
 
+    print()
     return iterations, width, height, state
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Game of Life implementation')
+    parser.add_argument('--pretty', dest='curses', action='store_true')
+    parser.set_defaults(curses=False)
+    args = parser.parse_args()
+
+    main(args)
